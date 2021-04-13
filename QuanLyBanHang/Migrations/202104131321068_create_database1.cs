f72a117112aba4c961eb94315e0a839fd5e06c05
thanh-cong-998 <cong30071998@gmail.com>
2021-04-13 20:21:46 +0700

    up

---
+++ QuanLyBanHang/Migrations/202104131321068_create_database1.cs
@@ -0,0 +1,16 @@
+namespace QuanLyBanHang.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create_database1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
